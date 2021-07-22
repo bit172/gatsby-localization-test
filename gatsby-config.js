e35b84b319d5394f0e647cf04ca2db361e704acd
Jonathan Hsu <bit172@gmail.com>
2021-07-21 23:52:11 -0400

    using react-i18n plugin add translations add lang switcher

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,41 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Gatsby_Localization",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/locales`,
+        name: `locale`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-react-i18next`,
+      options: {
+        localeJsonSourceName: `locale`,
+        languages: [`en`, `fr`],
+        defaultLanguage: `en`,
+        siteUrl: `http://localhost:8000/`,
+        i18nextOptions: {
+          interpolation: {
+            escapeValue: false,
+          },
+          keySeparator: false,
+          nsSeparator: false,
+        },
+        pages: [
+          {
+            matchPath: "/:lang?/blog/:uid",
+            getLanguageFromPath: true,
+            excludeLanguages: ["es"],
+          },
+          {
+            matchPath: "/preview",
+            languages: ["en"],
+          },
+        ],
+      },
+    },
+    "gatsby-plugin-gatsby-cloud",
+  ],
 };
